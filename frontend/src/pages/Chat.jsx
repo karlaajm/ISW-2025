ee09cb3e236b5379edee291b6ad14f3f50c689a6
VictorLopezG <victor.lopez2101@alumnos.ubiobio.cl>
2025-05-14 09:16:58 -0400

    primer commit

---
+++ frontend/src/pages/Chat.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "@styles/chat.css";
+
+//const socket = io("http://localhost:3000");
+const socket = io("/");
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", receiveMessage)
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>{
+    setMessages(state => [message, ...state]);
+    //console.log("Mensaje recibido");
+  }
+    
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      body: message,
+      from: JSON.parse(sessionStorage.getItem('usuario'))?.nombreCompleto,
+    };
+    setMessages(state => [newMessage, ...state]);
+    setMessage("");
+    socket.emit("message", newMessage);
+  };
+
+  return (
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+                }`}
+            >
+              <b>{message.from}</b>:{message.body}
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
+export default Chat;
