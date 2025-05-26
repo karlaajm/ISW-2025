10d00222e28d59614cd052c3ab7fe65d348e7de4
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2025-05-25 21:01:18 -0400

    fix rutas estudiante

--- backend/src/middlewares/authorization.middleware.js
+++ backend/src/middlewares/authorization.middleware.js
@@ -1,40 +1,6 @@
-import User from "../entity/user.entity.js";
+import Estudiante from "../entities/Estudiante.js";
 import { AppDataSource } from "../config/configDb.js";
 import {
 handleErrorClient,
 handleErrorServer,
 } from "../handlers/responseHandlers.js";
-
-export async function isAdmin(req, res, next) {
-try {
-    const userRepository = AppDataSource.getRepository(User);
-
-    const userFound = await userRepository.findOneBy({ email: req.user.email });
-
-    if (!userFound) {
-    return handleErrorClient(
-        res,
-        404,
-        "Usuario no encontrado en la base de datos",
-    );
-    }
-
-    const rolUser = userFound.rol;
-
-    if (rolUser !== "administrador") {
-        return handleErrorClient(
-            res,
-            403,
-            "Error al acceder al recurso",
-            "Se requiere un rol de administrador para realizar esta acción."
-        );
-    }
-    next();
-} catch (error) {
-    handleErrorServer(
-    res,
-    500,
-    error.message,
-    );
-}
-}
